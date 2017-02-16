5e9d7baa6241c9187e5d185e549eacda3666ee08
teenoyzz <bookclub_@hotmail.com>
2017-02-16 12:11:23 +0700

    Initial commit

---
+++ app/src/main/java/comsci/teenoyzz/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package comsci.teenoyzz.myapplication;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickMoreInfo(View view){
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.dlt.go.th/th/dlt-knowledge/view.php?_did=111"));
+        startActivity(intent);
+
+    }//method onclickmoreinfo
+
+
+
+}//main method
